5f6632065bf29e7665b1111203f19b3b0ab7f7f8
don <donfreiday@email.com>
2017-03-25 20:34:17 -0400

    Added code to demonstrate calling scanf.

--- test.asm
+++ test.asm
@@ -8,15 +8,24 @@ includelib legacy_stdio_definitions.lib
 
 ; ExitProcess PROTO return:DWORD
 extern printf:NEAR
+extern scanf:NEAR
 
 .data
-text BYTE "Hello world", 0
+input_format BYTE "%d",0
+text BYTE "You entered: %d", 0
+integer DWORD ?
 
 .code
 main PROC C                    ; Specify "C" calling convention
+	push offset input_format
+	push offset integer
+	call scanf
+	add esp,8
+
+	push offset integer
     push offset text
-    call printf
-    add  esp, 4
+	call printf
+    add  esp,8
 ;   invoke ExitProcess,0       ; Since the C library called main (this function)
                                ; we can set eax to 0 and use ret to have
                                ; the C runtime close down and return our error
